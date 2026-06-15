5432f6fd1c4133221c6c79d5c28174476b197aeb
SamsterBirdies <71206056+SamsterBirdies@users.noreply.github.com>
2026-06-15 03:04:55 -0700

    swarm changes

--- EXPLOSIONS/weapons/projectile_list.lua
+++ EXPLOSIONS/weapons/projectile_list.lua
@@ -6,14 +6,26 @@ table.insert(Sprites,
 	{
 		Normal = { Frames = 
 		{ 
-			--{ texture = path .. "/effects/media/bloom1.png" , colour = { 0.85, 0.80, 0.5, 0.3 },},
-			--{ texture = path .. "/effects/media/bloom1.png" , colour = { 0.9, 0.85, 0.3, 0.2 },},
-			--{ texture = path .. "/effects/media/bloom1.png" , colour = { 1, 0.85, 0.2, 0.1 },},
-			--{ texture = path .. "/effects/media/bloom1.png" , colour = { 0.9, 0.82, 0.3, 0.2 },},
 			{ texture = path .. "/effects/media/bloom1.png" , colour = { 0.85, 0.70, 0.2, 0.35 },},
 			{ texture = path .. "/effects/media/bloom1.png" , colour = { 0.9, 0.75, 0.1, 0.25 },},
 			{ texture = path .. "/effects/media/bloom1.png" , colour = { 1, 0.75, 0.0, 0.15 },},
 			{ texture = path .. "/effects/media/bloom1.png" , colour = { 0.9, 0.72, 0.1, 0.25 },},
+			duration = 0.04,
+			NextState = "Normal",
+		},},
+	},
+})
+table.insert(Sprites,
+{
+	Name = "sb_bloom_swarm2",
+	States =
+	{
+		Normal = { Frames = 
+		{ 
+			{ texture = path .. "/effects/media/bloom1.png" , colour = { 1, 1, 1, 1 },},
+			{ texture = path .. "/effects/media/bloom1.png" , colour = { 1, 1, 1, 0.6 },},
+			{ texture = path .. "/effects/media/bloom1.png" , colour = { 1, 1, 1, 0.3 },},
+			{ texture = path .. "/effects/media/bloom1.png" , colour = { 1, 1, 1, 0.75 },},
 			duration = 0.04,
 			NextState = "Normal",
 		},},
@@ -139,18 +151,22 @@ for k, v in pairs(Projectiles) do
 		}
 	elseif sbeat(v.SaveName, "laser") then
 		v.ProjectileSprite = "sb_bloom_plasma"
-	--[[elseif sbeat(v.SaveName, "missile") then
+	elseif sbeat(v.SaveName, "missile") then
+		if #v.Projectile.Root.ChildrenInFront == 2 then
+			table.remove(v.Projectile.Root.ChildrenInFront, 2)
+		end
 		table.insert(v.Projectile.Root.ChildrenInFront,
 		{
 			Name = "Bloom",
 			Angle = 0,
 			Offset = { 0, 0.25 },
-			Pivot = { 0, 0.25 },
+			Pivot = { 0, 0.2 },
 			PivotOffset = { 0, 0 },
-			Sprite = "sb_bloom_swarm",
+			Sprite = "sb_bloom_swarm2",
 			Additive = true,
-			Scale = 4,
-		})]]
+			Scale = 2.5,
+			Colour = { 255, 164, 24, 128},
+		})
 	elseif sbeat(v.SaveName, "missile2") then
 		--v.ProjectileSplashMaxForce = 400000
 		table.insert(v.Projectile.Root.ChildrenInFront,
